57cad9e78df341478ea24a10fa1327d73b1181da
FuzzyHobbit <fuzzyhobbit@gmx.com>
2018-07-14 21:13:11 -0400

    Update base58 prefixes

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -129,9 +129,9 @@ public:
         vSeeds.emplace_back("tango.prettyblock.com");
         vSeeds.emplace_back("delta.prettyblock.com");
 
-        base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,0);
-        base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,5);
-        base58Prefixes[SECRET_KEY] =     std::vector<unsigned char>(1,128);
+        base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,60);
+        base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,85);
+        base58Prefixes[SECRET_KEY] =     std::vector<unsigned char>(1,188);
         base58Prefixes[EXT_PUBLIC_KEY] = {0x04, 0x88, 0xB2, 0x1E};
         base58Prefixes[EXT_SECRET_KEY] = {0x04, 0x88, 0xAD, 0xE4};
 
